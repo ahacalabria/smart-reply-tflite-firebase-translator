321fdb3c5b049c37d770ff9829011b4bd42947ea
mrmint <nas@cesar.school>
2019-09-27 16:26:08 -0300

    Fluxo: do TextToSpeech para o SmartReply. UI Upgrad

--- app/src/main/java/com/example/android/smartreply/MainActivity.java
+++ app/src/main/java/com/example/android/smartreply/MainActivity.java
@@ -13,13 +13,19 @@ limitations under the License.
 package com.example.android.smartreply;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 //import androidx.FloatingActionButton;
+import android.speech.RecognizerIntent;
 import android.text.DynamicLayout;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -41,16 +47,10 @@ import java.util.Locale;
  * received.
  */
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "SmartReplyDemo";
     private SmartReplyClient client;
-
-    private Button sendButton;
-    private TextView messageTextView;
-    private EditText messageInput;
-    private FloatingActionButton tempButton;
-
     private Handler handler;
-
     private ArrayList<String> s_words;
     private Translate tradutor;
     private ArrayList<TextView> sugg = new ArrayList<>();
@@ -60,40 +60,37 @@ public class MainActivity extends AppCompatActivity {
     private TextView suggestion4;
     private TextView suggestion5;
     private TextView suggestion6;
+    private static final int NUBER_OF_SUGGESTIONS = 5;
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private EditText meuAudioEmTexto;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.tradutor = Translate.getInstance(this);
-        Log.v(TAG, "onCreate");
         setContentView(R.layout.main_activity);
-        messageInput = findViewById(R.id.message_input);
         suggestion1 = findViewById(R.id.suggestion1);
         suggestion2 = findViewById(R.id.suggestion2);
         suggestion3 = findViewById(R.id.suggestion3);
         suggestion4 = findViewById(R.id.suggestion4);
         suggestion5 = findViewById(R.id.suggestion5);
         suggestion6 = findViewById(R.id.suggestion6);
+        meuAudioEmTexto = findViewById(R.id.theTextToSpeechTextView);
 
+        // Negocios do SmartReply
         client = new SmartReplyClient(getApplicationContext());
         handler = new Handler();
-
-        sendButton = findViewById(R.id.send_button);
-        sendButton.setOnClickListener(
-                (View v) -> {
-                    send(messageInput.getText().toString());
-                });
-
-        //tempButton = findViewById(R.id.floatButton);
         s_words = new ArrayList<String>();
-
         sugg.add(suggestion1);
         sugg.add(suggestion2);
         sugg.add(suggestion3);
         sugg.add(suggestion4);
         sugg.add(suggestion5);
         sugg.add(suggestion6);
+
+        // Deixa teclado sempre aberto
+        upKeyboard(meuAudioEmTexto);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -123,20 +120,72 @@ public class MainActivity extends AppCompatActivity {
                 () -> {
                     //passar uma string como entrada, o client SMART REPLY, uma lista de respostas 's_words', e a Main Activity
                     tradutor.traduzirPT_EN(new String[] {message}, client, s_words, this);
-
-                    messageInput.setText("");
-
+                    meuAudioEmTexto.setText("");
                 });
     }
 
     public void appendMessage(final String message, final int i) {
         handler.post(
                 () -> {
-                    if ( i <= 5 ) {
+                    if ( i <= NUBER_OF_SUGGESTIONS ) {
                         sugg.get(i).setText(message);
                     }
                 });
-
-
     }
+
+    //Deixa teclado sempre visivel
+    public void upKeyboard(View view) {
+        meuAudioEmTexto.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.showSoftInput(meuAudioEmTexto, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    //Metodos de ação de clique
+    public void ouvir(View view) {
+        upKeyboard(view);
+        limpar(view);
+        startVoiceInput();
+    }
+
+    public void limpar(View view) {
+        meuAudioEmTexto.setText("");
+        //textoSugeridoSelecionado.setText("");
+        suggestion1.setText("--");
+        suggestion2.setText("--");
+        suggestion3.setText("--");
+        suggestion4.setText("--");
+        suggestion5.setText("--");
+        suggestion6.setText("--");
+        //string = "";
+    }
+
+    //Metodos de audição
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Ouvindo...");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    meuAudioEmTexto.setText(result.get(0));
+                    send(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
 }
